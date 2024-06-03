8cf644e9a82ade67925a8c95610cf260535523c3
shingukim <minsukim@g.shingu.ac.kr>
2024-06-03 15:04:06 +0900

    2024. 6/3일까지 소스코드

---
+++ java-server2024/JavaServer/src/main/java/Hell1.java
@@ -0,0 +1,51 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hell1
+ */
+@WebServlet("/hell1")
+public class Hell1  extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hell1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#getServletInfo()
+	 */
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null; 
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ java-server2024/JavaServer/src/main/java/member/Member.java
@@ -0,0 +1,46 @@
+package member;
+
+import java.sql.Date;
+
+public class Member {
+	
+	private int id;
+	private String username;
+	private String company;
+	private Date birthday;
+	private String email;
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getid() {
+		return this.id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date brithday) {
+		this.birthday = brithday;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
